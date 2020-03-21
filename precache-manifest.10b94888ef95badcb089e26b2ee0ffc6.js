336db37e2c9ee1330f33d7cdb309af81e5dd3bc1
juaneg <juaaneg@gmail.com>
2020-03-21 17:19:03 -0300

    Updates

---
+++ precache-manifest.10b94888ef95badcb089e26b2ee0ffc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9aee3c3f50b71946771af9e24c31be1",
+    "url": "/RecipesApp/index.html"
+  },
+  {
+    "revision": "ef73e1f900c0d0fa6fcf",
+    "url": "/RecipesApp/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "ab0fb10d217c7a7c43ea",
+    "url": "/RecipesApp/static/css/main.fccea131.chunk.css"
+  },
+  {
+    "revision": "ef73e1f900c0d0fa6fcf",
+    "url": "/RecipesApp/static/js/2.a1e822ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipesApp/static/js/2.a1e822ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0fb10d217c7a7c43ea",
+    "url": "/RecipesApp/static/js/main.e1b46abe.chunk.js"
+  },
+  {
+    "revision": "ae8ec24521cca04fc384",
+    "url": "/RecipesApp/static/js/runtime-main.f97d5c09.js"
+  }
+]);
